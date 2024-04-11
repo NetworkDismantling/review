7d097214b54dfa554f5c166e4bf53eaf755bc82b
Marco Grassia <666024+eMarco@users.noreply.github.com>
2024-04-11 13:27:49 +0200

    Update table_output.py

--- network_dismantling/table_output.py
+++ network_dismantling/table_output.py
@@ -16,7 +16,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with the code.  If not, see <http://www.gnu.org/licenses/>.
 
-
+from argparse import ArgumentParser
 from pathlib import Path
 
 import matplotlib.pyplot as plt
